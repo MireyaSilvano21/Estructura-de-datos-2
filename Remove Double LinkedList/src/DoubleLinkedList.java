63ecfe5a00cbb7546b35cf801cf35edd36a5a301
MireyaSilvano21 <20243ds132@utez.edu.mx>
2025-10-16 18:49:24 -0600

    Metodo remove de DoubleLinkedList

---
+++ Remove Double LinkedList/src/DoubleLinkedList.java
@@ -0,0 +1,71 @@
+public class DoubleLinkedList {
+
+    Node head;
+    public void add(int data){
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode; //Guarda el nuevo nodo
+        newNode.prev = current;//En el nunevo nodo, guarda la referencia del anterior
+    }
+    public void printForward(){
+        if (head == null) {
+            System.out.println("La lista esta vacia");
+            return;
+        }
+        Node current = head;
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+    public void printBackward(){
+         if (head == null) {
+            System.out.println("La lista essta vacia");
+            return;
+        }
+         Node current = head;
+        while (current.next != null) { 
+            current = current.next;
+        }
+        while (current != null) {
+            System.out.println(current.data);
+            current = current.prev;
+        }
+        System.out.println("null");
+    }
+
+    public void remove(int data){
+        if (head== null) {
+            System.out.println("La lista esta vacia");
+            return;  
+        }
+        Node current = head;
+        while (current != null && current.data != data) {
+            current = current.next;
+        }
+        if (current == null) {
+            return;
+        }
+        if (current == head) {
+            head = current.next;
+            if (head != null) {
+                head.prev = null;
+            }
+        }
+        if (current.prev != null) {
+            current.prev.next = current.next;
+        }
+        if (current.next != null) {
+            current.next.prev = current.prev;
+        }
+    }
+
+}
